96d133d05b5d0fee8a04f39325ab33d0050302df
Teodora Vrabevska <thedito89@gmail.com>
2025-03-02 16:42:55 +0200

    Added new ts exercises

---
+++ arrays.ts
@@ -0,0 +1,11 @@
+//Arrays
+
+let numbersArray: number[] = [1, 2, 3, 4];
+let stringAndNumsArr: (number | string)[] = ["Pesho", "Tosho", "2", "3"];
+
+let fruits: string[] = ["Strawberry", "Pear", "Apple", "Banana"];
+let fruitsEl: string = fruits[1];
+let secondEl: string = fruits[3];
+
+fruits.push("Mango");
+console.log("firstEl")
--- index.ts
+++ index.ts
@@ -1,33 +1,46 @@
 // Let vs Const
 let number: number = 5;
 number = 10;
-const message: string = 'Welcome'
+const message: string = 'Welcome' 
+
 // String
-let welcomeMessage: string = "Hello, Peter!"
-let test: string = 'Ivan';
-let welcomeMessageLength: number = welcomeMessage.length;
-// String Interpolation
-let welcomeMessageDetails: string = `My Welcome Message length is ${welcomeMessageLength}`
+let wellcomeMessage: string = "Hello, Peter!";
+let test: string = "Ivo";
+
+let wellcomeMessageLenght: number = wellcomeMessage.length;
+
+// String interpolation
+
+let wellcomeMessageDetiles: string = 'My wlecome message lenght is ${wellcomeMessage}';
 let userWelcomeMessage: string = `Welcome, ${test}`
+
 // Number
 let firstNumber: number = 5;
 let secondNumber: number = 10;
-// Number Math Operations
-let add: number = secondNumber + firstNumber;
-let substract: number = secondNumber - firstNumber;
-let multiply: number = secondNumber * firstNumber;
-let divide: number = secondNumber / firstNumber;
-// Boolean (true/false)
+
+//Number math operations
+
+let add:number = firstNumber + secondNumber;
+let substract: number = firstNumber - secondNumber;
+let multiply: number = firstNumber * secondNumber;
+let dicide: number = firstNumber / secondNumber;
+
+//Boolean (true/false)
+
 let isGreater: boolean = firstNumber > secondNumber;
 let isLower: boolean = firstNumber < secondNumber;
 let isEqual: boolean = firstNumber === secondNumber;
 let isGreaterOrEqual: boolean = firstNumber >= secondNumber;
-let isLowerOrEqual: boolean = firstNumber <= secondNumber;
+let isLowerOrEqual: boolean = firstNumber  <= secondNumber;
+
 let areNumbersEqualTo: boolean = 5 === firstNumber && 10 === secondNumber;
-let isAnyNumberEqualTo: boolean = 6 === firstNumber || 10 === secondNumber;
-// Any
-let stringValue: any = 'Hello';
+let isAnyNumberEqualTo: boolean = 6 === firstNumber || 11 === secondNumber;
+
+
+//Any
+let stringValue: any = "Hello";
 stringValue = 10;
 stringValue = 10 === 10;
 stringValue = [10, 20, 30];
-console.log("stringValue", stringValue);
+
+console.log("stringValue",stringValue);
